4dba407110e5363df840e637b1b2e24a15789dc8
Lautaro Vallejos <lautaro.vallejos123@hotmail.com>
2021-12-30 03:03:09 -0300

    First elements from Homepage finished

--- test/Dapp/Dapp/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Planets(models.Model):
-    id = models.PrimaryKey()
-    name = models.CharField(max_length=50)
-    house = models.IntegerField()
--- test/Dapp/Dapp/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from datetime import datetime
-
-def saludo(request):
-    now = str(datetime.now())
-    return HttpResponse(f"Soy bastante grande amigo y mido: {now}")
